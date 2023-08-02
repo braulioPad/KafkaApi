f3162e8937471258c46ee20d1d41abc34f269c04
braulioPad <64759065+braulioPad@users.noreply.github.com>
2023-08-02 14:46:34 +0900

    change get method to post method

--- src/main/java/com/main/controller/MessageController.java
+++ src/main/java/com/main/controller/MessageController.java
@@ -1,7 +1,7 @@
 package com.main.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +18,9 @@ public class MessageController {
 		this.kafkaProducer = kafkaProducer;
 	}
 
-	// http:localhost:8080/api/v1/kafka/publish?message=hello world
-	@GetMapping("/publish")
-	public ResponseEntity<String> publish(@RequestParam("message") String message) {
+	// http:localhost:8080/api/v1/kafka/publish
+	@PostMapping("/publish")
+	public ResponseEntity<String> publish(@RequestParam("post_param") String message) {
 		kafkaProducer.sendMessage(message);
 		return ResponseEntity.ok("Message sent to the topic");
 	}
